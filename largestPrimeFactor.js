720383afe751950825efc4b8900c4a25125060a8
Jan Phillip Carreon <janphillipcarreon@Jans-MacBook-Pro.local>
2016-09-08 13:00:21 -1000

    all numbers

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,11 +1,18 @@
 exports.largestPrimeFactor = function(n){
 
   var divisor =2;
-  while(n%divisor!==0){
-    divisor++;
-   while(n%divisor===0 && divisor<(n/2)){
+  if (n%divisor===0){
+    while(n%divisor===0 && divisor<(n/2)){
       n/=divisor;
-  }
-}
+      }
+        }else{
+
+    while(n%divisor!==0){
+      divisor++;
+        while(n%divisor===0 && divisor<(n/2)){
+        n/=divisor;
+        }
+      }
+      }
   return n;
-};
+  };
